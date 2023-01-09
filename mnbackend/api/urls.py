034d6e3554c99de79cf6cca5af9227a9f9745122
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-01-08 23:12:16 -0800

    removed

--- mnbackend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-path('posts/', views.PostListCreate.as_view()),
-path('posts/<int:pk>',views.PostRetrieveUpdateDestroy.as_view()),
-]
