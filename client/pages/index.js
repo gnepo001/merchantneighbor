f547a36df67bb262b09fa76327c2b2487a2ff548
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-01-08 23:10:37 -0800

    created frontend for project

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const index = () => {
+  return (
+    <div className="bg-white text-black">
+      <div className="">
+        <div>MerchantNeighbor</div>
+        <div>Find the items your looking for at low prices!</div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
