bcfe1566e6133274c36324b879f8d9b4c5dfb8a7
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-01-10 21:27:57 -0800

    added price attrib to Post Model

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -7,4 +7,4 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id','title','description','created','sold']
+        fields = ['id','title','description','created','price','sold']
--- server/post/models.py
+++ server/post/models.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
     description = models.TextField(blank=True)
    
     created = models.DateTimeField(auto_now_add=True)
     sold = models.BooleanField(default=False)
+
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
