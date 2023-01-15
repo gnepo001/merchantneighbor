0f7fbebc5af33036db4994d64526b5bb9ebf4e46
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-01-14 23:32:01 -0800

    render post.image field

--- client/components/Posts.js
+++ client/components/Posts.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Post = ({ title, description, price }) => {
+const Post = ({ title, image, description, price }) => {
   return (
     <div className="flex flex-col w-1/5">
       <h1>{title}</h1>
+      {image ? (
+        <div className="h-full w-full">
+          <img src={image} />
+        </div>
+      ) : (
+        <div className="h-full w-full">
+          <h1 className="text-center mt-[40%]">No Image</h1>
+        </div>
+      )}
       <div>{price}</div>
       <p className="text-gray-400 text-sm">{description}</p>
     </div>
@@ -17,6 +26,7 @@ const Posts = ({ posts }) => {
         <Post
           key={post.id}
           title={post.title}
+          image={post.image}
           price={post.price}
           description={post.description}
         />
