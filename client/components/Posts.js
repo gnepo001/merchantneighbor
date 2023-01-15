99718bea24e166c072a20423043b763317ae1bbd
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-01-15 01:16:32 -0800

    UI changes

--- client/components/Posts.js
+++ client/components/Posts.js
@@ -2,17 +2,15 @@ import React from "react";
 
 const Post = ({ title, image, description, price }) => {
   return (
-    <div className="flex flex-col w-1/5">
-      <h1>{title}</h1>
+    <div className="flex flex-col mx-5 mt-5">
       {image ? (
-        <div className="h-full w-full">
-          <img src={image} />
-        </div>
+        <img className="object-cover h-48 w-96" src={image} />
       ) : (
         <div className="h-full w-full">
           <h1 className="text-center mt-[40%]">No Image</h1>
         </div>
       )}
+      <h1 className="font-bold">{title}</h1>
       <div>{price}</div>
       <p className="text-gray-400 text-sm">{description}</p>
     </div>
@@ -21,7 +19,7 @@ const Post = ({ title, image, description, price }) => {
 
 const Posts = ({ posts }) => {
   return (
-    <div className="flex flex-row justify-around">
+    <div className="grid grid-cols-4 gap-4 place-items-stretch ">
       {posts.map((post) => (
         <Post
           key={post.id}
