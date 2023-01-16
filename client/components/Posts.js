77bbcd1b22be54cc745e99f88f72f31565f07867
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-01-15 23:10:23 -0800

    UI Highlight Changes

--- client/components/Posts.js
+++ client/components/Posts.js
@@ -1,14 +1,18 @@
 import React from "react";
 import format from "number-format.js";
+import Link from "next/link";
 
 const Post = ({ title, image, description, price }) => {
   return (
-    <div className="flex flex-col mx-5 mt-5">
+    <div className="flex flex-col mx-3 mt-5 cursor-pointer">
       {image ? (
-        <img className="object-cover h-48 w-96 rounded-lg" src={image} />
+        <img
+          className="object-cover h-48 w-96 rounded-lg hover:opacity-60 transition duration-300 ease-in-out"
+          src={image}
+        />
       ) : (
-        <div className="h-full w-full">
-          <h1 className="text-center mt-[40%]">No Image</h1>
+        <div className="h-full w-full bg-slate-200 rounded-lg hover:opacity-60 transition duration-300 ease-in-out">
+          <h1 className="text-center py-20">No Image</h1>
         </div>
       )}
       <h1 className="font-bold">{title}</h1>
@@ -18,7 +22,7 @@ const Post = ({ title, image, description, price }) => {
         <div>{format("$ #,###.00", price, { enforceMaskSign: true })}</div>
       )}
 
-      <p className="text-gray-400 text-sm">{description}</p>
+      <p className="text-gray-400 text-sm ">{description}</p>
     </div>
   );
 };
@@ -27,13 +31,19 @@ const Posts = ({ posts }) => {
   return (
     <div className="grid grid-cols-4 gap-4 place-items-stretch ">
       {posts.map((post) => (
-        <Post
+        <Link
           key={post.id}
-          title={post.title}
-          image={post.image}
-          price={post.price}
-          description={post.description}
-        />
+          href={{
+            pathname: `/posts/${post.id}`,
+          }}
+        >
+          <Post
+            title={post.title}
+            image={post.image}
+            price={post.price}
+            description={post.description}
+          />
+        </Link>
       ))}
     </div>
   );
