e0bdd435606959f8b981157bd515868c015738eb
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-01-15 23:09:45 -0800

    Changed UI

--- client/components/Header.js
+++ client/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="pt-3 flex flex-row justify-between items-center">
       <div className="flex flex-col text-[#0FE2D5] text-center w-1/5 ml-5">
         <div className="font-bold text-3xl">MerchantNeighbor</div>
-        <div className="text-xs">Find things you need Near you!</div>
+        <div className="text-xs">Find things you need, Near you!</div>
       </div>
       <div className="flex flex-row justify-around font-bold text-lg w-1/5">
         <div>Cart</div>
