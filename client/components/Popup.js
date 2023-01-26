e46d7b85ad277810f233ca9e9cda21baead7dc22
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-01-25 22:46:42 -0800

    sign up option added to login

--- client/components/Popup.js
+++ client/components/Popup.js
@@ -1,44 +1,105 @@
-import React from "react";
-import { TiDelete } from "react-icons/ti";
+import React, { useState } from "react";
+
+const Signup = ({ ctp }) => {
+  return (
+    <div>
+      <div className="flex flex-row ml-40 justify-between w-1/2 ">
+        <h1 className="text-center pt-5 text-2xl">Sign Up</h1>
+        {/* sends state back to the parent div */}
+        <button className="text-gray-500" onClick={() => ctp(false)}>
+          Cancel
+        </button>
+      </div>
+      <div className="font-bold text-3xl text-[#0FE2D5] text-center mt-4">
+        MerchantNeighbor
+      </div>
+      <form className="flex flex-col w-2/3 mx-auto mt-6">
+        <label htmlFor="email">Email</label>
+        <input
+          className="bg-white border-2 rounded-lg"
+          type="text"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="email">Password</label>
+        <input
+          className="bg-white border-2 rounded-lg"
+          type="password"
+          id="password"
+          name="password"
+        />
+        <label htmlFor="email">Confirm Password</label>
+        <input
+          className="bg-white border-2 rounded-lg"
+          type="cpassword"
+          id="cpassword"
+          name="cpassword"
+        />
+        <input
+          className="bg-[#0FE2D5] rounded-xl mt-5"
+          type="submit"
+          value="Sign Up"
+        />
+      </form>
+    </div>
+  );
+};
+
+const Login = ({ ctp }) => {
+  return (
+    <div>
+      <div className="flex flex-row ml-40 justify-between w-1/2 ">
+        <h1 className="text-center pt-5 text-2xl">Login In</h1>
+        {/* sends state back to the parent div */}
+        <button className="text-gray-500" onClick={() => ctp(false)}>
+          Cancel
+        </button>
+      </div>
+      <div className="font-bold text-3xl text-[#0FE2D5] text-center mt-4">
+        MerchantNeighbor
+      </div>
+      <form className="flex flex-col w-2/3 mx-auto mt-6">
+        <label htmlFor="email">Email</label>
+        <input
+          className="bg-white border-2 rounded-lg"
+          type="text"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="email">Password</label>
+        <input
+          className="bg-white border-2 rounded-lg"
+          type="password"
+          id="password"
+          name="password"
+        />
+        <input
+          className="bg-[#0FE2D5] rounded-xl mt-5"
+          type="submit"
+          value="Log In"
+        />
+      </form>
+    </div>
+  );
+};
 
 const Popup = ({ ctp }) => {
+  const [isRegistered, setIsRegistered] = useState(true);
+
   return (
     <div className="absolute w-screen h-screen top-0 bg-[#00000066] -translate-x-[40%] pt-28">
       <div className="bg-white z-100 rounded-lg w-1/3 h-3/5 mx-auto mt-22 max-w-md">
-        <div className="flex flex-row ml-40 justify-between w-1/2 ">
-          <h1 className="text-center pt-5 text-2xl">Login In</h1>
-          {/* sends state back to the parent div */}
-          <button className="text-gray-500" onClick={() => ctp(false)}>
-            Cancel
-          </button>
-        </div>
-        <div className="font-bold text-3xl text-[#0FE2D5] text-center mt-4">
-          MerchantNeighbor
-        </div>
-        <form className="flex flex-col w-2/3 mx-auto mt-6">
-          <label for="email">Email</label>
-          <input
-            className="bg-white border-2 rounded-lg"
-            type="text"
-            id="email"
-            name="email"
-          />
-          <label for="email">Password</label>
-          <input
-            className="bg-white border-2 rounded-lg"
-            type="password"
-            id="password"
-            name="password"
-          />
-          <input
-            className="bg-[#0FE2D5] rounded-xl mt-5"
-            type="submit"
-            value="Log In"
-          />
-        </form>
-        <div className="text-gray-500 font-sans w-1/2 mx-auto text-sm mt-3">
-          Don't Have an Account? Create One here
-        </div>
+        {isRegistered ? <Login ctp={ctp} /> : <Signup ctp={ctp} />}
+        <button
+          onClick={() => setIsRegistered(!isRegistered)}
+          className="text-gray-500 font-sans w-full text-sm mt-3"
+        >
+          {isRegistered ? (
+            <div>Don't Have an Account? Create One here</div>
+          ) : (
+            <div>Already have an account? Login In</div>
+          )}
+        </button>
       </div>
     </div>
   );
