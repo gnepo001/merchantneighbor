4b2224a9e7e0ff483d57a7e9797aa13939dda9e8
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-01-27 22:50:19 -0800

    Custom user bug fixed, was not allowing client side to connect properly

--- server/accounts/admin.py
+++ server/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+#from django.contrib.auth import get_user_model
 
 #CustomUser = get_user_model()
 
--- server/accounts/models.py
+++ server/accounts/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
-    pass
     class Meta:
         db_table = 'auth_user'
