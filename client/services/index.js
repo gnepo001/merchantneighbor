5d5845b7d3dc5a958de24142c65c6f07e32908d1
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-02-07 20:27:55 -0800

    authorization token added

--- client/services/index.js
+++ client/services/index.js
@@ -7,3 +7,13 @@ export const signup = (data) => {
 export const login = (data) => {
   return axios.post("http://localhost:8000/api/login/", data);
 };
+
+export const createPost = (data, token) => {
+  return axios
+    .post("http://localhost:8000/api/createPost/", data, {
+      headers: { Authorization: `Token ${token}` },
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
