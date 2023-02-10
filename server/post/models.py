9db77e1b04e2572074ddf21670a66b64b88a25fb
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-02-09 23:25:33 -0800

    edited post model for handling post creation from client side

--- server/post/models.py
+++ server/post/models.py
@@ -1,25 +1,32 @@
 from django.db import models
 from accounts.models import CustomUser
 
+# have learned so much in django, when trying to change fields from the client side
+# make sure you are edititng the fields and not properties
+# after many many days on the problem finally understood the problem
+# the differences, it is good to struggle with porblems best way 
+# to learn. I kept trying to change the user below when it should have been 
+# the userkey as that is a field and user is a property
+
 class Post(models.Model):
     #foriegn keys
     userkey = models.ForeignKey(CustomUser, on_delete=models.CASCADE) 
 
     #properties
-    title = models.CharField(max_length=100)
-    image= models.FileField(upload_to='files/', null=True, verbose_name="") #image field
-    price = models.IntegerField(default=0)
+    title = models.CharField(max_length=100,blank=True)
+    image= models.FileField(upload_to='files/', null=True, verbose_name="",blank=True) #image field
+    price = models.IntegerField(default=0,blank=True)
     description = models.TextField(blank=True)
     likes = models.IntegerField(default=0)
-    tags = models.CharField(max_length=30,blank=False,default="tag1,tag2,...") #sqlite doesnt support arrays
-    date_modified = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-    sold = models.BooleanField(default=False)
+    tags = models.CharField(max_length=30,blank=True,default="tag1,tag2,...") #sqlite doesnt support arrays
+    date_modified = models.DateTimeField(auto_now=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True,blank=True)
+    sold = models.BooleanField(default=False,blank=True)
 
     @property
     def user(self):
         return self.userkey.username
-
+        
     @property
     def datejoined(self):
         return self.userkey.date_joined
