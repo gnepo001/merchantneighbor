6d426cfb70d92dbe664f85a319ebb3927d617aea
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-02-14 22:24:41 -0800

    created URL to collect same user products

--- server/api/urls.py
+++ server/api/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 path('posts/', views.GetAllPosts.as_view()), #GET all posts in database
 path('posts/<int:pk>',views.GetSinglePost.as_view()), 
+path('creatorAllPosts/<str:user>',views.CreatorAllPosts.as_view()),
 path('createPost/',views.CreatePost.as_view()),
 #path('posts/<int:pk>',views.PostRetrieveUpdateDestroy.as_view()), 
 #path('posts/<int:pk>/sold',views.PostToggleSold.as_view()), #mark posts as sold
